5fbb2caa7ed07a287c88213976d06cd55e3cdf41
Doksanbir <yalcindoksanbir@gmail.com>
2021-04-22 11:09:09 +0300

    Initial commit

---
+++ src/test/java/com/example/projectmanagementreact/ProjectManagementReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectmanagementreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
